3946cc3d62f0452761e6e4422675e9c919b6453d
Lucas Fonseca <lucas.b4rn@gmail.com>
2019-01-23 15:44:57 -0200

    Criando variaveis e verificando a tipagem das variaveis

---
+++ 2-CriandoVariaveis/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_CriandoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int idade = 10;
+            double idade1 = 25;
+            long idade2 = 30;
+            String idade3 = "30";
+
+
+            int varRecebeDouble = (int) idade1;
+
+            int varRecebeDouble1 = Convert.ToInt16(idade3);
+
+
+
+
+            Console.WriteLine(varRecebeDouble1 + varRecebeDouble);
+            
+
+            Console.WriteLine("Executando projeto 2 - Criando Variveis");
+            Console.WriteLine("Execução finalizada. tecle para continua!!");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
